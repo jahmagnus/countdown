2081a134e2b296a6a6f278ac80f5152e418fcf6e
Inge <jahmagnus@gmail.com>
2021-03-28 20:02:11 +0100

    ...

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express()
+
+
+//create instance of the server
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`listning on port ${port}`)
+})
