c5e6cfe93ed3efb9cf9a4a8d99a65a654bbfba12
Inge <jahmagnus@gmail.com>
2021-03-28 20:20:25 +0100

    ...

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log(`listning on port ${port}`)
 })
+
+
+//host pages from the public folder
+
+app.use(express.static("public"))
+
+
+
+//serve up the countdown page as the default loading page
+
+app.get("/", (req, res) =>{
+    res.redirect('index.html')
+})
