e3dc0554dc01ad59d0a98f4007361c7263eef8ab
Inge <jahmagnus@gmail.com>
2021-03-28 20:56:38 +0100

    ...

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,33 @@
 
 
+
+function timer(){
 //set the date counting down to
-const countdownDate = new Date("May 01, 2022 12:00:00").getDate()
+const countdownDate = new Date("May 01, 2022 12:00:00").getTime()
+
+
+//update count every second
+let x = setInterval(function(){
+
+    //get today's date and time
+    let now = new Date().getTime()
+
+    //get a distance value between now and the countdown date
+    let distance = countdownDate - now
+
+    //calculations for days, hours, minutes, seconds
+    let days = Math.floor(distance/(1000*60*60*24))
+    let hours = Math.floor((distance%(1000*60*60*24)) / (1000 * 60 * 60))
+    let mins = Math.floor((distance%(1000*60*60*24)) / (1000 * 60))
+    let secs = Math.floor((distance%(1000*60*60*24)) / 1000)
+
+    console.log(days, hours, mins, secs)
+
+    
+
+})
+
+}
+
+
+timer()
